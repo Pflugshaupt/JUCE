5b80fc509159c33e147cb16a927a1f3b39014f43
Adrian Pflugshaupt <adi@pflugshaupt.com>
2025-02-10 10:36:24 +0100

    PathStrokeStype::createDashedStroke() do not call startNewSubPath for every gap step.

--- modules/juce_graphics/geometry/juce_PathStrokeType.cpp
+++ modules/juce_graphics/geometry/juce_PathStrokeType.cpp
@@ -713,11 +713,13 @@ void PathStrokeType::createDashedStroke (Path& destPath,
                 return;
             }
 
-            if (isSolid && ! first)
-                newDestPath.lineTo (it.x1, it.y1);
-            else
-                newDestPath.startNewSubPath (it.x1, it.y1);
-
+            if (isSolid) {
+                if (!first)
+                    newDestPath.lineTo (it.x1, it.y1);
+                else
+                    newDestPath.startNewSubPath (it.x1, it.y1);
+            }
+            
             dx = it.x2 - it.x1;
             dy = it.y2 - it.y1;
             lineLen = juce_hypot (dx, dy);
